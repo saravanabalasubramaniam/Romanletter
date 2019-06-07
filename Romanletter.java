259099f4994eb69f2d1468777248fde627d1b66c
saravana <saravanasaru14313@gmail.com>
2019-06-07 12:12:03 +0530

    Update and rename README.md to Romanletter.java

---
+++ Romanletter.java
@@ -0,0 +1,92 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        if(str.equals("I"))
+        {
+            System.out.println("1");
+        }
+        else if(str.equals("II"))
+        {
+            System.out.println("2");
+        }
+        else if(str.equals("III"))
+        {
+            System.out.println("3");
+        }
+        else if(str.equals("IV"))
+        {
+            System.out.println("4");
+        }
+        else if(str.equals("V"))
+        {
+            System.out.println("5");
+        }
+        else if(str.equals("VI"))
+        {
+            System.out.println("6");
+        }
+       else  if(str.equals("VII"))
+        {
+            System.out.println("7");
+        }
+        else if(str.equals("VIII"))
+        {
+            System.out.println("8");
+        }
+        else if(str.equals("IX"))
+        {
+            System.out.println("9");
+        }
+        else if(str.equals("X"))
+        {
+            System.out.println("10");
+        }
+        else if(str.equals("XI"))
+        {
+            System.out.println("11");
+        }
+        else if(str.equals("XII"))
+        {
+            System.out.println("12");
+        }
+        else if(str.equals("XIII"))
+        {
+            System.out.println("13");
+        }
+        else if(str.equals("XIV"))
+        {
+            System.out.println("14");
+        }
+        else if(str.equals("XV"))
+        {
+            System.out.println("15");
+        }
+        else if(str.equals("XVI"))
+        {
+            System.out.println("16");
+        }
+        else if(str.equals("XVII"))
+        {
+            System.out.println("17");
+        }
+        else if(str.equals("XVIII"))
+        {
+            System.out.println("18");
+        }
+        else if(str.equals("XIX"))
+        {
+            System.out.println("19");
+        }
+        else if(str.equals("XX"))
+        {
+            System.out.println("20");
+        }
+        else
+        {
+        }
+	}
+}
